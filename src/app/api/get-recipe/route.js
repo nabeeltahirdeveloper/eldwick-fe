b4754a69c56bfc23294f3448aaf56bcb75bcc02d
Nabeel <nabeeltahirdeveloper@gmail.com>
2023-11-04 04:36:10 +0500

    again fix

--- src/app/api/get-recipe/route.js
+++ src/app/api/get-recipe/route.js
@@ -1,10 +1,18 @@
-import { NextResponse } from "next/server";
+// pages/api/recipes.js
+
 import clientPromise from "@/lib/mongodb";
 
-export async function GET() {
-  const client = await clientPromise;
-  const db = client.db();
+export default async function handler(req, res) {
+  if (req.method === 'GET') {
+    try {
+      const client = await clientPromise;
+      const db = client.db('recipes'); // Replace with your database name
 
-  const result = await db.collection("recipes").find().toArray();
-  return NextResponse.json(result);
+      const recipes = await db.collection("recipes").find().toArray();
+      return res.status(200).json(recipes);
+    } catch (e) {
+      // Error handling
+      return res.status(500).json({ error: e.message });
+    }
+  }
 }
