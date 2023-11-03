129397ccdfcdbfb9a4423c991cda3d48411f9e87
Nabeel <nabeeltahirdeveloper@gmail.com>
2023-11-04 04:31:47 +0500

    fx

--- src/app/api/get-recipe/route.js
+++ src/app/api/get-recipe/route.js
@@ -1,11 +1,10 @@
 import { NextResponse } from "next/server";
 import clientPromise from "@/lib/mongodb";
 
-const client = await clientPromise;
-const db = await client.db();
-
 export async function GET() {
-    const result = await db.collection("recipes").find().toArray();
-    return NextResponse.json(result);
-    }
-    
+  const client = await clientPromise;
+  const db = client.db();
+
+  const result = await db.collection("recipes").find().toArray();
+  return NextResponse.json(result);
+}
