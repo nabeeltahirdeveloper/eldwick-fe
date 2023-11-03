7ea56bada75dc0f444dc315204b32aedd498eac6
Nabeel <nabeeltahirdeveloper@gmail.com>
2023-11-04 03:24:01 +0500

    get

--- src/app/page.js
+++ src/app/page.js
@@ -14,7 +14,7 @@ export default function Home() {
   const updateRecipe =async (recipe) => {
     console.log("recipe", recipe);
     const result = await axios.post(
-      'https://eldwick-fe.vercel.app//api/update-recipe',
+      'https://eldwick-fe.vercel.app/api/update-recipe',
       recipe
     );
     setRecipes(recipes.map((r) => (r._id === recipe._id ? recipe : r)));
@@ -22,7 +22,7 @@ export default function Home() {
 
   async function fetchData() {
     const result = await axios.get(
-      'https://eldwick-fe.vercel.app//api/get-recipe'
+      'https://eldwick-fe.vercel.app/api/get-recipe'
     );
     setRecipes(result.data);
   }
