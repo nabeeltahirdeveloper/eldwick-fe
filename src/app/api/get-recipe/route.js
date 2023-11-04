463832a50dbc3477f607d500387c61e0095978c0
Nabeel <nabeeltahirdeveloper@gmail.com>
2023-11-04 05:00:43 +0500

    middle ware fix

--- src/app/api/get-recipe/route.js
+++ src/app/api/get-recipe/route.js
@@ -1,13 +1,11 @@
 import { NextResponse } from "next/server";
 import clientPromise from "@/lib/mongodb";
 
-export async function GET() {
-  const client = await clientPromise;
-  const db = client.db();
 
-  const result = await db.collection("recipes").find().toArray();
-  
-  return NextResponse.json(result).headers({
-    "Cache-Control": "s-maxage=60, stale-while-revalidate",
-  });
-}
+export async function GET() {
+    const client = await clientPromise;
+    const db = await client.db();
+    const result = await db.collection("recipes").find().toArray();
+    return NextResponse.json(result);
+    }
+    
