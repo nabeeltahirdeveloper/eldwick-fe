9148d87cd869d87b06e5e76e114653bc6e040b87
Nabeel <nabeeltahirdeveloper@gmail.com>
2023-11-04 05:13:05 +0500

    next fix

--- src/app/api/get-recipe/route.js
+++ src/app/api/get-recipe/route.js
@@ -5,16 +5,26 @@ import clientPromise from "@/lib/mongodb";
 export async function GET() {
     const client = await clientPromise;
     const db = await client.db();
-    const result = await db.collection("recipes").find().toArray();
-    const response = new NextResponse(JSON.stringify(result), {
-        
-        headers: {
-            'Content-Type': 'application/json',
-            'Cache-Control': 'no-store, max-age=0'
-        },
-    });
+     // Add try-catch to handle any potential errors
+     try {
+        const recipes = await db.collection("recipes").find().toArray();
+        console.log("Recipes fetched:", recipes); // Log fetched data for debugging
 
-    return response;
-
+        return new NextResponse(JSON.stringify(recipes), {
+            status: 200,
+            headers: {
+                'Content-Type': 'application/json',
+                'Cache-Control': 'no-store, max-age=0',
+            },
+        });
+    } catch (error) {
+        console.error("Failed to fetch recipes:", error); // Error logging
+        return new NextResponse(JSON.stringify({ error: error.message }), {
+            status: 500,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
     }
+}
     
